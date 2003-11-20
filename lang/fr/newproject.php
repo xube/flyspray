39eb8f9ea8ea4376ca6b8ba2feeb649051052984
knigits <knigits@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2003-11-20 13:15:01 +0000

    Oops!  I forgot to add this new file to the French translation. All fixed now.

---
+++ lang/fr/newproject.php
@@ -0,0 +1,22 @@
+<?php
+
+$newproject_text = array (
+
+'createnewproject'	=>	'Créer un nouveau projet',
+'projecttitle'		=>	'Titre du projet',
+'themestyle'		=>	'Thême / Style',
+'projectprefs'		=>	'Préférences du projet',
+'defaultproject'	=>	'Projet par défaut',
+'projectlists'		=>	'Liste des projets',
+'addnewproject'		=>	'Créer un nouveau projet',
+'showlogo'			=>	'Montrer le logo',
+'intromessage'		=>	'Message d\'introduction',
+'htmlallowed'		=>	'Code HTML autorisé',
+'createproject'		=>	'Créer un nouveau projet',
+'noone'				=>	'Personne',
+'defaultcatowner'	=>	'Propriétaire par défaut de la catégorie',
+'createthisproject'	=>	'Créer ce projet',
+'nopermission'		=>	'Vous n\'avez pas l\'autorisation d\'accéder à cette page',
+
+);
+?>
