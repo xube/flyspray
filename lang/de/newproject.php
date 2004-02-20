17ffa087591f2d5edd7c7b88e17c797c4c8f222c
knigits <knigits@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2004-02-20 02:35:40 +0000

    Completed German translation.

---
+++ lang/de/newproject.php
@@ -0,0 +1,22 @@
+<?php
+
+$newproject_text = array (
+
+'createnewproject'	=>	'Ein neues Projekt erstellen',
+'projecttitle'		=>	'Projekt Titel',
+'themestyle'		=>	'Erscheinungsbild',
+'projectprefs'		=>	'Projekt-Einstellungen',
+'defaultproject'	=>	'Standard Projekt',
+'projectlists'		=>	'Projekt-Listen',
+'addnewproject'		=>	'Neues Projekt hinzufügen',
+'showlogo'			=>	'Titel-Logo anzeigen',
+'intromessage'		=>	'Einführungs-Nachricht',
+'htmlallowed'		=>	'HTML-Code zulassen',
+'createproject'		=>	'Neues Projekt erstellen',
+'noone'				=>	'Niemand',
+'defaultcatowner'	=>	'Standard Kategorie-Inhaber',
+'createthisproject'	=>	'Dieses Projekt erstellen',
+'nopermission'		=>	'Sie sind nicht berechtigt diese Seite zu nutzen',
+
+);
+?>
