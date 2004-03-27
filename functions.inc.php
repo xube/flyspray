4a6de30222d7c71ff3333add7becde7b4940046c
knigits <knigits@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2004-03-27 02:43:33 +0000

    Implemented task #204.  Haven't tested it yet.

--- functions.inc.php
+++ functions.inc.php
@@ -19,7 +19,7 @@ function get_language_pack($lang, $module) {
 
 
 class Flyspray {
-    var $version = '0.9.5';
+    var $version = '0.9.6 (devel)';
 
    function getGlobalPrefs() {
       $get_prefs = $this->dbQuery("SELECT pref_name, pref_value FROM flyspray_prefs");
@@ -60,16 +60,16 @@ class Flyspray {
 
    /* Replace undef values (treated as NULL in SQL database) with empty
    strings.
-   @param arr	input array or false
-   @return	SQL safe array (without undefined values)
+   @param arr        input array or false
+   @return        SQL safe array (without undefined values)
    */
    function dbUndefToEmpty($arr) {
        if (is_array($arr)) {
-	   $c = count($arr);
+           $c = count($arr);
 
-	   for($i=0; $i<$c; $i++)
-	       if (!isset($arr[$i])) 
-		   $arr[$i] = '';
+           for($i=0; $i<$c; $i++)
+               if (!isset($arr[$i]))
+                   $arr[$i] = '';
        }
        return $arr;
    }
@@ -78,42 +78,42 @@ class Flyspray {
     checkboxes.
     */
     function emptyToZero($arg) {
-	return empty($arg) ? 0 : $arg;
+        return empty($arg) ? 0 : $arg;
     }
 
    function dbExec($sql, $inputarr=false, $numrows=-1, $offset=-1) {
       // replace undef values (treated as NULL in SQL database) with empty
       // strings
       $inputarr = $this->dbUndefToEmpty($inputarr);
-       
+
       $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
       if (($numrows>=0) or ($offset>=0)) {
-	  $result =  $this->dblink->SelectLimit($sql, $numrows, $offset, $inputarr);
+          $result =  $this->dblink->SelectLimit($sql, $numrows, $offset, $inputarr);
       } else {
-	  $result =  $this->dblink->Execute($sql, $inputarr);
+          $result =  $this->dblink->Execute($sql, $inputarr);
       }
       if (!$result) {
-	  if (function_exists("debug_backtrace")) {
-	      echo "<pre style='text-align: left;'>";
-	      var_dump(debug_backtrace());
-	      echo "<pre>";
-	  }
-	  
-	  die (sprintf("Query {%s} with params {%s} Failed! (%s)", 
-		    $sql, implode(', ', $inputarr), 
-		    $this->dblink->ErrorMsg()));
+          if (function_exists("debug_backtrace")) {
+              echo "<pre style='text-align: left;'>";
+              var_dump(debug_backtrace());
+              echo "<pre>";
+          }
+
+          die (sprintf("Query {%s} with params {%s} Failed! (%s)",
+                    $sql, implode(', ', $inputarr),
+                    $this->dblink->ErrorMsg()));
       }
       return $result;
    }
 
    function dbCountRows($result) {
       $num_rows = $result->RecordCount();
-   	return $num_rows;
+           return $num_rows;
    }
 
    function dbFetchRow(&$result) {
       $row = $result->FetchRow();
-   	return $row;
+           return $row;
    }
 
 /* compatibility functions */
@@ -134,35 +134,35 @@ function GetTaskDetails($task_id) {
    $flyspray_prefs = $this->GetGlobalPrefs();
    $lang = $flyspray_prefs['lang_code'];
 
-	$get_details = $this->dbQuery("SELECT *,
-						vr.version_name as reported_version_name,
-						vd.version_name as due_in_version_name
-						FROM flyspray_tasks t
-						LEFT JOIN flyspray_projects p ON t.attached_to_project = p.project_id
-						LEFT JOIN flyspray_list_category c ON t.product_category = c.category_id
-						LEFT JOIN flyspray_list_os o ON t.operating_system = o.os_id
-						LEFT JOIN flyspray_list_resolution r ON t.resolution_reason = r.resolution_id
-						LEFT JOIN flyspray_list_tasktype tt ON t.task_type = tt.tasktype_id
-						LEFT JOIN flyspray_list_version vr ON t.product_version = vr.version_id
-						LEFT JOIN flyspray_list_version vd ON t.closedby_version = vd.version_id
-   
-						WHERE t.task_id = ?
-						", array($task_id));
-	$get_details = $this->dbFetchArray($get_details);
+        $get_details = $this->dbQuery("SELECT *,
+                                                vr.version_name as reported_version_name,
+                                                vd.version_name as due_in_version_name
+                                                FROM flyspray_tasks t
+                                                LEFT JOIN flyspray_projects p ON t.attached_to_project = p.project_id
+                                                LEFT JOIN flyspray_list_category c ON t.product_category = c.category_id
+                                                LEFT JOIN flyspray_list_os o ON t.operating_system = o.os_id
+                                                LEFT JOIN flyspray_list_resolution r ON t.resolution_reason = r.resolution_id
+                                                LEFT JOIN flyspray_list_tasktype tt ON t.task_type = tt.tasktype_id
+                                                LEFT JOIN flyspray_list_version vr ON t.product_version = vr.version_id
+                                                LEFT JOIN flyspray_list_version vd ON t.closedby_version = vd.version_id
+
+                                                WHERE t.task_id = ?
+                                                ", array($task_id));
+        $get_details = $this->dbFetchArray($get_details);
     if (empty($get_details))
            $get_details = array();
 
-	$status_id = $get_details['item_status'];
+        $status_id = $get_details['item_status'];
     require("lang/$lang/status.php");
-	$tmp_array = array("status_name" => $status_list[$status_id]);
-	$get_details = $get_details + $tmp_array;
+        $tmp_array = array("status_name" => $status_list[$status_id]);
+        $get_details = $get_details + $tmp_array;
 
-	$severity_id = $get_details['task_severity'];
+        $severity_id = $get_details['task_severity'];
     require("lang/$lang/severity.php");
-	$tmp_array = array("severity_name" => $severity_list[$severity_id]);
-	$get_details = $get_details + $tmp_array;
+        $tmp_array = array("severity_name" => $severity_list[$severity_id]);
+        $get_details = $get_details + $tmp_array;
 
-	return $get_details;
+        return $get_details;
 }
 
 
@@ -205,14 +205,14 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
 
    // End of checking that jabber is set up
    }
-   
+
    //return TRUE;
 }
 
-   function SendEmail($to, $message) {
+   /*function SendEmail($to, $message) {
 
        if (empty($to))
-	   return;
+           return;
 
    $flyspray_prefs = $this->GetGlobalPrefs();
 
@@ -228,8 +228,8 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
        $from = "From: {$flyspray_prefs['project_title']} <{$flyspray_prefs['admin_email']}>";
        $useragent = "User-Agent: Mutt";
 
-	   $message = str_replace('&amp;', '&', $message);
-	   $message = "
+           $message = str_replace('&amp;', '&', $message);
+           $message = "
 ========================================================
 {$functions_text['autogenerated']}
 ========================================================
@@ -241,7 +241,7 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
               "$contenttype\r\n"
               ."$from\r\n"
               ."$useragent");
-	 };
+         };
 
    } else {
 
@@ -251,8 +251,8 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
        $from = "From: {$flyspray_prefs['project_title']} <{$flyspray_prefs['admin_email']}>";
        $useragent = "User-Agent: Mutt";
 
-	   $message = str_replace('&amp;', '&', $message);
-	   $message = "
+           $message = str_replace('&amp;', '&', $message);
+           $message = "
 ========================================================
 {$functions_text['autogenerated']}
 ========================================================
@@ -265,57 +265,78 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
               ."$from\r\n"
               ."$useragent");
 
-	};
-	//return TRUE;
+        };
+        //return TRUE;
    // End of mass mail function
+   }*/
+
+   function SendEmail($to, $message) {
+   if (empty($to)){
+     return;
+   } elseif (is_array($to)) {
+     $to = implode(",", $to);
    }
+
+ $flyspray_prefs = $this->GetGlobalPrefs();
+ require('lang/'.$flyspray_prefs['lang_code'].'/functions.inc.php');
+ $subject = $functions_text['notifyfrom'].' '.$flyspray_prefs['project_title'];
+ $message = str_replace('&amp;', '&', $message);
+ $message = $functions_text['autogenerated']."\n".str_repeat('-', 72)."\n\n".$message;
+ $message = wordwrap($message, 72);
+ $headers = array();
+ $headers[] = "Content-Type: text/plain; charset=UTF-8";
+ $headers[] = trim('From: '.$flyspray_prefs['project_title'].' <'.$flyspray_prefs['admin_email'].'>');
+ $headers[] = "User-Agent: Mutt";
+ $headers = implode("\n", $headers);
+ mail($to, $subject, $message, $headers);
+}
 
    // This function sends out basic messages at specified times.
    function SendBasicNotification($to, $message) {
 
-	if (empty($to))
-	    return;
+        if (empty($to))
+            return;
 
        $flyspray_prefs = $this->GetGlobalPrefs();
 
        $lang = $flyspray_prefs['lang_code'];
        require("lang/$lang/functions.inc.php");
 
-	   $get_user_details = $this->dbQuery("SELECT real_name, jabber_id, email_address, notify_type FROM flyspray_users WHERE user_id = ?", array($to));
-	   list($real_name, $jabber_id, $email_address, $notify_type) = $this->dbFetchArray($get_user_details);
+           $get_user_details = $this->dbQuery("SELECT real_name, jabber_id, email_address, notify_type FROM flyspray_users WHERE user_id = ?", array($to));
+           list($real_name, $jabber_id, $email_address, $notify_type) = $this->dbFetchArray($get_user_details);
 
-	   // if app preferences say to use jabber, or if the user can (and has) selected jabber
-	   // and the jabber options are entered in the applications preferences
-	   if (($flyspray_prefs['user_notify'] == '3')
-	     OR ($flyspray_prefs['user_notify'] == '1' && $notify_type == '2')
+           // if app preferences say to use jabber, or if the user can (and has) selected jabber
+           // and the jabber options are entered in the applications preferences
+           if (($flyspray_prefs['user_notify'] == '3')
+             OR ($flyspray_prefs['user_notify'] == '1' && $notify_type == '2')
 
-		 && $flyspray_prefs['jabber_server'] != ''
-	     && $flyspray_prefs['jabber_port'] != ''
-	     && $flyspray_prefs['jabber_username'] != ''
-	     && $flyspray_prefs['jabber_password'] != ''
-	     ) {
+                 && $flyspray_prefs['jabber_server'] != ''
+             && $flyspray_prefs['jabber_port'] != ''
+             && $flyspray_prefs['jabber_username'] != ''
+             && $flyspray_prefs['jabber_password'] != ''
+             ) {
 
-	     $message = stripslashes($message);
+             $message = stripslashes($message);
 
-		$this->JabberMessage(
-		  				$flyspray_prefs['jabber_server'],
-						$flyspray_prefs['jabber_port'],
-						$flyspray_prefs['jabber_username'],
-						$flyspray_prefs['jabber_password'],
-						$jabber_id,
-						"{$functions_text['notifyfrom']} {$flyspray_prefs['project_title']}",
-						$message,
-						"Flyspray"
-						);
-		//return TRUE;
+                $this->JabberMessage(
+                                                  $flyspray_prefs['jabber_server'],
+                                                $flyspray_prefs['jabber_port'],
+                                                $flyspray_prefs['jabber_username'],
+                                                $flyspray_prefs['jabber_password'],
+                                                $jabber_id,
+                                                "{$functions_text['notifyfrom']} {$flyspray_prefs['project_title']}",
+                                                $message,
+                                                "Flyspray"
+                                                );
+                //return TRUE;
 
-	    // if app preferences say to use email, or if the user can (and has) selected email
-		} elseif (($flyspray_prefs['user_notify'] == '2') OR ($flyspray_prefs['user_notify'] == '1' && $notify_type == '1')) {
+            // if app preferences say to use email, or if the user can (and has) selected email
+                } elseif (($flyspray_prefs['user_notify'] == '2') OR ($flyspray_prefs['user_notify'] == '1' && $notify_type == '1')) {
 
-			$to = "$real_name <$email_address>";
-			$this->SendEmail($to, $message);
-		};
-	// End of basic notification function
+                        $to = "$real_name <$email_address>";
+                        $this->SendEmail($to, $message);
+                };
+        // End of basic notification function
    }
 
 
@@ -323,54 +344,54 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
     // These are the additional people who want to be notified of a task changing
     function SendDetailedNotification($task_id, $message) {
 
-	$flyspray_prefs = $this->GetGlobalPrefs();
+        $flyspray_prefs = $this->GetGlobalPrefs();
 
-	$lang = $flyspray_prefs['lang_code'];
-	require("lang/$lang/functions.inc.php");
+        $lang = $flyspray_prefs['lang_code'];
+        require("lang/$lang/functions.inc.php");
 
-	$jabber_users = array();
-	$email_users = array();
+        $jabber_users = array();
+        $email_users = array();
 
-	$get_users = $this->dbQuery("SELECT user_id FROM flyspray_notifications WHERE task_id = ?", array($task_id));
+        $get_users = $this->dbQuery("SELECT user_id FROM flyspray_notifications WHERE task_id = ?", array($task_id));
 
-	while ($row = $this->dbFetchArray($get_users)) {
+        while ($row = $this->dbFetchArray($get_users)) {
 
-	    $get_details = $this->dbQuery("SELECT notify_type, jabber_id, email_address
-		    FROM flyspray_users
-		    WHERE user_id = ?",
-		    array($row['user_id']));
-	    while ($subrow = $this->dbFetchArray($get_details)) {
+            $get_details = $this->dbQuery("SELECT notify_type, jabber_id, email_address
+                    FROM flyspray_users
+                    WHERE user_id = ?",
+                    array($row['user_id']));
+            while ($subrow = $this->dbFetchArray($get_details)) {
 
-		if (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '1')
-			OR ($flyspray_prefs['user_notify'] == '2')) {
-		    array_push($email_users, $subrow['email_address']);
-		} elseif (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '2')
-			OR ($flyspray_prefs['user_notify'] == '3')) {
-		    array_push($jabber_users, $subrow['jabber_id']);
-		};
-	    };
-	};
+                if (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '1')
+                        OR ($flyspray_prefs['user_notify'] == '2')) {
+                    array_push($email_users, $subrow['email_address']);
+                } elseif (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '2')
+                        OR ($flyspray_prefs['user_notify'] == '3')) {
+                    array_push($jabber_users, $subrow['jabber_id']);
+                };
+            };
+        };
 
-	$message = stripslashes($message);
+        $message = stripslashes($message);
 
-	// Pass the recipients and message onto the Jabber Message function
-	$this->JabberMessage(
-		$flyspray_prefs['jabber_server'],
-		$flyspray_prefs['jabber_port'],
-		$flyspray_prefs['jabber_username'],
-		$flyspray_prefs['jabber_password'],
-		$jabber_users,
-		"{$functions_text['notifyfrom']} {$flyspray_prefs['project_title']}",
-		$message,
-		"Flyspray"
-		);
+        // Pass the recipients and message onto the Jabber Message function
+        $this->JabberMessage(
+                $flyspray_prefs['jabber_server'],
+                $flyspray_prefs['jabber_port'],
+                $flyspray_prefs['jabber_username'],
+                $flyspray_prefs['jabber_password'],
+                $jabber_users,
+                "{$functions_text['notifyfrom']} {$flyspray_prefs['project_title']}",
+                $message,
+                "Flyspray"
+                );
 
 
-	// Pass the recipients and message onto the mass email function
-	$this->SendEmail($email_users, $message);
+        // Pass the recipients and message onto the mass email function
+        $this->SendEmail($email_users, $message);
 
-	//return TRUE;
-	// End of detailed notification function
+        //return TRUE;
+        // End of detailed notification function
     }
 
 
@@ -381,9 +402,9 @@ function JabberMessage( $sHost, $sPort, $sUsername, $sPassword, $vTo, $sSubject,
 
       $these_groups = explode(" ", $flyspray_prefs['assigned_groups']);
       while (list($key, $val) = each($these_groups)) {
-	if (empty($val)) 
-	  continue;
-	$group_details = $this->dbFetchArray($this->dbQuery("SELECT group_name FROM flyspray_groups WHERE group_id = ?", array($val)));
+        if (empty($val))
+          continue;
+        $group_details = $this->dbFetchArray($this->dbQuery("SELECT group_name FROM flyspray_groups WHERE group_id = ?", array($val)));
 
         echo "<optgroup label=\"{$group_details['group_name']}\">\n";
 
