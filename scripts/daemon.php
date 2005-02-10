e9bcd670220639cbf0d545617689e550da212881
knigits <knigits@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-02-10 05:53:00 +0000

    Added missing background daemon script for the php 'cron' solution. Thanks again Eelco!

---
+++ scripts/daemon.php
@@ -0,0 +1,24 @@
+<?php
+
+$include = $_SERVER['argv'][1];
+$sleep   = $_SERVER['argv'][2];
+$runfile = $_SERVER['argv'][3];
+
+chdir(dirname(__FILE__));
+
+while (touch($runfile))
+{
+    run($include);
+    sleep($sleep);
+}
+
+function run($include)
+{
+    global $db, $fs;
+    
+    $lang = 'en';
+
+    include $include;
+}
+
+?>
