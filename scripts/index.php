4439ab0507391cebd1c8a65ea20919e927625638
mac <mac@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-26 23:09:01 +0000

    Updates to 0.9.8 'stable'
    FS#708: This modifies the last-minute fix that made it into r371 to allow 
    logged in users to see projects that have others_view=1. The change 
    inadvertently showed all projects names to all users, but this corrects that.
    
    FS#572: Reports using a date range were not including events happening on 
    the ending date. This corrects that, and also makes any missing range 
    endpoints assume today.

--- scripts/index.php
+++ scripts/index.php
@@ -129,14 +129,16 @@ if (isset($_GET['project']) && $_GET['project'] == '0')
    // Those who aren't super users get this more restrictive query
    } elseif (isset($_COOKIE['flyspray_userid']))
    {
-       $check_projects = $db->Query("SELECT p.project_id
-                                     FROM {$dbprefix}users_in_groups uig
-                                     LEFT JOIN {$dbprefix}groups g ON uig.group_id = g.group_id,
-                                     {$dbprefix}projects p
-                                     WHERE ((uig.user_id = ?
-                                     AND g.view_tasks = '1')
-                                     OR p.others_view = '1')
-                                     AND p.project_is_active = '1'
+       $check_projects = $db->Query("SELECT p.* FROM {$dbprefix}projects p
+                                     LEFT JOIN {$dbprefix}groups g
+                                     ON p.project_id=g.belongs_to_project
+                                     AND g.view_tasks=1
+                                     LEFT JOIN {$dbprefix}users_in_groups uig
+                                     ON uig.group_id = g.group_id
+                                     AND uig.user_id = ?
+                                     WHERE p.project_is_active='1'
+                                     AND (p.others_view
+                                     OR uig.user_id IS NOT NULL)
                                      ORDER BY p.project_title",
                                      array($current_user['user_id'])
                                    );
--- scripts/reports.php
+++ scripts/reports.php
@@ -353,9 +353,12 @@ switch ($_REQUEST['sort']) {
         $date = 'from';
         $fromdate = $_REQUEST['fromdate'];
         $todate = $_REQUEST['todate'];
-
+	if (!isset($fromdate) || $fromdate=="") { $fromdate = date("d-M-Y"); }
+	if (!isset($todate) || $todate=="") { $todate = date("d-M-Y"); }
+	
         $ufromdate = strtotime($fromdate);
-        $utodate = strtotime($todate);
+	// Add 24 hours to the end to make it include that date
+        $utodate = strtotime($todate) + 86400;
 
         $wheredate = "AND h.event_date > {$ufromdate} AND h.event_date < {$utodate}";
         break;
