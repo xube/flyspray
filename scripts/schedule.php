9e8ecfccb82e7cecdfe5a9fca689d2af89e7184d
MadCoder <MadCoder@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-30 10:14:21 +0000

    scripts/schedule.php:
        sql lint, prefer $arra[] = than array_push and minor things like that.
    
    
        made me think that we have a potential security breach with all that
        accessible things under scripts/ ...

--- scripts/schedule.php
+++ scripts/schedule.php
@@ -1,76 +1,68 @@
 <?php
 // This script checks for pending scheduled notifications
 // and sends them at the right time.
-include_once('../header.php');
-include_once('../includes/notify.inc.php');
+$path = dirname(dirname(__FILE__));
+require_once("$path/header.php");
+require_once("$path/includes/notify.inc.php");
+
 $notify = new Notifications;
 
 $now = date(U);
 
-$get_reminders = $db->Query("SELECT * FROM {$dbprefix}reminders r
-                             LEFT JOIN {$dbprefix}tasks t ON r.task_id = t.task_id
-                             LEFT JOIN {$dbprefix}projects p ON t.attached_to_project = p.project_id
-                             WHERE t.is_closed = '0'
-                             ORDER BY r.reminder_id");
+$get_reminders = $db->Query("SELECT  * FROM {$dbprefix}reminders r
+                          LEFT JOIN  {$dbprefix}tasks t ON r.task_id = t.task_id
+                          LEFT JOIN  {$dbprefix}projects p ON t.attached_to_project = p.project_id
+                              WHERE  t.is_closed = '0'
+                           ORDER BY  r.reminder_id");
 
-// Cycle through all reminders in the database table
-while ($row = $db->FetchRow($get_reminders))
-{
-   // Check to see if it's time to send a reminder
-   if (($row['start_time'] < $now) && (($row['last_sent'] + $row['how_often']) < $now))
-   {
-      // Send the reminder
-      $lang = $flyspray_prefs['lang_code'];
-      require("../lang/$lang/functions.inc.php");
+while ($row = $db->FetchRow($get_reminders)) {
+    // Check to see if it's time to send a reminder
+    if (($row['start_time'] < $now) && (($row['last_sent'] + $row['how_often']) < $now)) {
+        // Send the reminder
+        $lang = $flyspray_prefs['lang_code'];
+        require_once("$path/lang/$lang/functions.inc.php");
 
-      $jabber_users = array();
-      $email_users = array();
+        $jabber_users = array();
+        $email_users  = array();
 
-      // Get the user's notification type and address
-      $get_details = $db->Query("SELECT notify_type, jabber_id, email_address
-                                 FROM {$dbprefix}users
-                                 WHERE user_id = ?",
-                                 array($row['to_user_id']));
+        // Get the user's notification type and address
+        $get_details  = $db->Query("SELECT  notify_type, jabber_id, email_address
+                                      FROM  {$dbprefix}users
+                                     WHERE  user_id = ?", array($row['to_user_id']));
 
-      while ($subrow = $db->FetchArray($get_details))
-      {
-         if (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '1')
-            OR ($flyspray_prefs['user_notify'] == '2'))
-         {
-            array_push($email_users, $subrow['email_address']);
+        while ($subrow = $db->FetchArray($get_details)) {
+            if (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '1')
+                    OR ($flyspray_prefs['user_notify'] == '2'))
+            {
+                $email_users[] = $subrow['email_address'];
 
-         } elseif (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '2')
-            OR ($flyspray_prefs['user_notify'] == '3'))
-         {
-            array_push($jabber_users, $subrow['jabber_id']);
-         }
-      }
+            }
+            elseif (($flyspray_prefs['user_notify'] == '1' && $subrow['notify_type'] == '2')
+                    OR ($flyspray_prefs['user_notify'] == '3'))
+            {
+                $jabber_users[] = $subrow['jabber_id']);
+            }
+        }
 
-      $subject = $functions_text['notifyfrom'];
-      $message = stripslashes($row['reminder_message']);
+        $subject = $functions_text['notifyfrom'];
+        $message = stripslashes($row['reminder_message']);
 
-      // Pass the recipients and message onto the notification function
-      $notify->SendEmail($email_users, $subject, $message);
-      $notify->StoreJabber($jabber_users, $subject, $message);
+        // Pass the recipients and message onto the notification function
+        $notify->SendEmail($email_users, $subject, $message);
+        $notify->StoreJabber($jabber_users, $subject, $message);
 
-      // Update the database with the time sent
-      $update_db = $db->Query("UPDATE {$dbprefix}reminders
-                               SET last_sent = ?
-                               WHERE reminder_id = ?",
-                               array($now, $row['reminder_id']));
-
-      // Debug
-      echo "Reminder Sent!<br>";
-
-   }
+        // Update the database with the time sent
+        $update_db = $db->Query("UPDATE  {$dbprefix}reminders
+                                    SET  last_sent = ?
+                                  WHERE  reminder_id = ?",
+                              array($now, $row['reminder_id']));
+    }
 }
 
 // send those stored notifications
 $notify->SendJabber();
 
-
 ?>
-
 <html>
 <head>
 <title>Scheduled Reminders</title>
