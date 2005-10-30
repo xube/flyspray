ab546d8d608f8ea47f9cab9592f3a0277bccdd61
MadCoder <MadCoder@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-30 10:15:23 +0000

    lint scripts/rss*.php
       essentially GPC class use (really powerfull here)
    
       and some pre-templatization

--- scripts/rss.php
+++ scripts/rss.php
@@ -1,92 +1,63 @@
 <?php
-// Include the header to set up database access etc
-include_once('../header.php');
+// We can't include this script as part of index.php?do= etc,
+// as that would introduce html code into it.  HTML != Valid XML
+// So, include the headerfile to set up database access etc
+require_once(dirname(dirname(__FILE__)).'/header.php');
 
-// If the user gives us a limit, use it.  default is five results
-if (isset($_REQUEST['num']) && is_numeric($_REQUEST['num']))
-{
-   $limit = $_REQUEST['num'];
-} else
-{
-   $limit = '10';
-}
+$limit = intval(Req::val('num', 10));
+$proj  = intval(Req::val('proj', $flyspray_prefs['default_project']));
 
-// If the user requested tasks from a specific project, select it. default is global default project
-if (isset($_REQUEST['proj']) && is_numeric($_REQUEST['proj']) && !empty($_REQUEST['proj']))
-{
-   $proj = $_REQUEST['proj'];
-} else
-{
-   $proj = $flyspray_prefs['default_project'];
-}
-
-switch ($_REQUEST['type'])
-{
+switch (Req::val('type')) {
    case 'new': $orderby = 'date_opened';
-               $title = 'Recently opened tasks';
+               $title   = 'Recently opened tasks';
    break;
    case 'clo': $orderby = 'date_closed';
-               $title = 'Recently closed tasks';
+               $title   = 'Recently closed tasks';
    break;
    case 'sev': $orderby = 'task_severity';
-               $title = 'Most severe tasks';
+               $title   = 'Most severe tasks';
    break;
    case 'pri': $orderby = 'task_priority';
-               $title = 'Priority tasks';
+               $title   = 'Priority tasks';
    break;
-   default: $orderby = 'date_opened';
-            $title = 'Recently opened tasks';
+   default:    $orderby = 'date_opened';
+               $title   = 'Recently opened tasks';
    break;
 }
 
 $project_prefs = $fs->getProjectPrefs($proj);
 
+$task_details = $db->Query("SELECT  task_id, item_summary, detailed_desc
+                              FROM  {$dbprefix}tasks t
+                         LEFT JOIN  {$dbprefix}projects p ON t.attached_to_project = p.project_id
+                             WHERE  t.is_closed <> '1' AND p.project_id = ?
+                                    AND p.project_is_active = '1' AND t.mark_private <> '1'
+                          ORDER BY  $orderby DESC", array($proj), $limit);
+
 // Set up the basic XML head
-header ("Content-type: text/xml");
-echo '<?xml version="1.0"?>' . "\n";
-echo '<rss version="2.0">';
-//echo '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">' . "\n";
-//echo '<channel rdf:about="http://www.zend.com/news.rss">' . "\n";
-echo '<channel>';
-echo '<title>Flyspray</title>' . "\n";
-echo '<description>Flyspray:: ' . $project_prefs['project_title'] . ': ' . $title . '</description>' . "\n";
-echo '<link>http://flyspray.rocks.cc/</link>' . "\n";
-//echo '<image rdf:resource="" />' . "\n";
+header ('Content-type: text/xml');
+echo '<?xml version="1.0"?>'."\n";
+?>
+<rss version="2.0">
+  <channel>
+    <title>Flyspray</title>
+    <description>Flyspray:: <?php echo $project_prefs['project_title'] . ': ' .  $title ?></description>
+    <link>http://flyspray.rocks.cc/</link>
+<?php
+        while ($row = $db->FetchArray($task_details)):
+            $item_summary  = htmlspecialchars($row['item_summary']);
+            $detailed_desc = htmlspecialchars($row['detailed_desc']);
 
-// Query the database
-$task_details = $db->Query("SELECT task_id, item_summary, detailed_desc
-                            FROM {$dbprefix}tasks t
-                            LEFT JOIN {$dbprefix}projects p ON t.attached_to_project = p.project_id
-                            WHERE t.is_closed <> '1'
-                            AND p.project_id = ?
-                            AND p.project_is_active = '1'
-                            AND t.mark_private <> '1'
-                            ORDER BY $orderby DESC",
-                            array($proj), $limit);
-
-
-// Now, let's loop the results
-while ($row = $db->FetchArray($task_details))
-{
-   $item_summary = htmlspecialchars($row['item_summary']);
-   $detailed_desc = htmlspecialchars($row['detailed_desc']);
-
-   if (!get_magic_quotes_gpc())
-   {
-      $item_summary = str_replace("\\", "&#92;", $item_summary);
-      $detailed_desc = str_replace("\\", "&#92;", $detailed_desc);
-   }
-
-   $item_summary = stripslashes($item_summary);
-   $detailed_desc = stripslashes($detailed_desc);
-
-   echo '<item>' . "\n";
-   echo '<title>' . $item_summary . '</title>' . "\n";
-   echo '<description>' . $fs->FormatText($detailed_desc) . '</description>' . "\n";
-   echo '<link>' . $fs->CreateURL('details', $row['task_id']) . '</link>' . "\n";
-   echo '</item>';
-}
-
-echo '</channel>' . "\n";
-//echo '</rdf:RDF>' . "\n";
-echo '</rss>';
+            if (get_magic_quotes_gpc()) {
+                $item_summary  = stripslashes($item_summary);
+                $detailed_desc = stripslashes($detailed_desc);
+            }
+?>
+    <item>
+      <title><?php echo $item_summary ?></title>
+      <description><?php echo $fs->FormatText($detailed_desc) ?></description>
+      <link><?php echo $fs->CreateURL('details', $row['task_id']) ?></link>
+    </item>
+<?php   endwhile; ?>
+  </channel>
+</rss>
