33dc622e4b18e22e979b6ec14db00baf6a7a26e6
MadCoder <MadCoder@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-30 10:11:57 +0000

    scripts/authenticate.php refactor
       usual tricks (see logs since rev 378)

--- scripts/authenticate.php
+++ scripts/authenticate.php
@@ -3,69 +3,62 @@
 
 $fs->get_language_pack($lang, 'authenticate');
 
-// If logout was requested, log the user out.
-if (isset($_REQUEST['action']) && $_REQUEST['action'] == "logout")
-{
-   // Set cookie expiry time to the past, thus removing them
-   setcookie('flyspray_userid', '', time()-60, '/');
-   setcookie('flyspray_passhash', '', time()-60, '/');
-   setcookie('flyspray_project', '', time()-60, '/');
-   if (isset($_COOKIE[session_name()])) {
-      setcookie(session_name(), '', time()-60, '/');
-   }
+if (Req::val('action') == 'logout') {
+    // If logout was requested, log the user out.
 
-   // Unset all of the session variables.
-   $_SESSION = array();
-   session_destroy();
-   $fs->redirect($conf['general']['baseurl']);
+    // Set cookie expiry time to the past, thus removing them
+    setcookie('flyspray_userid',   '', time()-60, '/');
+    setcookie('flyspray_passhash', '', time()-60, '/');
+    setcookie('flyspray_project',  '', time()-60, '/');
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-60, '/');
+    }
 
-// Otherwise, they requested login.  See if they provided the correct credentials...
-} elseif (isset($_REQUEST['user_name']) && isset($_REQUEST['password']) )
-{
-   $username = $_REQUEST['user_name'];
-   $password = $_REQUEST['password'];
-
-   // Run the username and password through the login checker
-   if (!$fs->checkLogin($username, $password))
-   {
-      $_SESSION['ERROR'] = $authenticate_text['loginfailed'];
-      $fs->redirect($_REQUEST['prev_page']);
-
-   } else
-   {
-      $user_id = $fs->checkLogin($username, $password);
-
-      // Determine if the user should be remembered on this machine
-      if (isset($_REQUEST['remember_login']) )
-      {
-         $cookie_time = time() + (60 * 60 * 24 * 30); // Set cookies for 30 days
-      } else
-      {
-         $cookie_time = 0; // Set cookies to expire when session ends (browser closes)
-      }
-
-      $user = $fs->getUserDetails($user_id);
-
-      // Set a couple of cookies
-      setcookie('flyspray_userid', $user['user_id'], $cookie_time, "/");
-      setcookie('flyspray_passhash', crypt($user['user_pass'], $cookiesalt), $cookie_time, "/");
-
-      // If the user had previously requested a password change, remove the magic url
-      $remove_magic = $db->Query("UPDATE {$dbprefix}users SET
-                                  magic_url = ''
-                                  WHERE user_id = ?",
-                                  array($user['user_id'])
-                                );
-
-      $_SESSION['SUCCESS'] = $authenticate_text['loginsuccessful'];
-      $fs->redirect($_REQUEST['prev_page']);
-   // End of checking credentials
-   }
-
-} else
-{
-   // If the user didn't provide both a username and a password, show this error:
-   $_SESSION['ERROR'] = $authenticate_text['loginfailed'] . ' - ' . $authenticate_text['userandpass'];
-   $fs->redirect($_REQUEST['prev_page']);
+    // Unset all of the session variables.
+    $_SESSION = array();
+    session_destroy();
+    $fs->redirect($conf['general']['baseurl']);
 }
+
+if (Req::has('user_name') && Req::has('password')) {
+    // Otherwise, they requested login.  See if they provided the correct credentials...
+    $username = Req::val('user_name');
+    $password = Req::val('password');
+
+    // Run the username and password through the login checker
+    if (!$fs->checkLogin($username, $password)) {
+        $_SESSION['ERROR'] = $authenticate_text['loginfailed'];
+        $fs->redirect($_REQUEST['prev_page']);
+    }
+    else {
+        $user_id = $fs->checkLogin($username, $password);
+
+        // Determine if the user should be remembered on this machine
+        if (Req::has('remember_login')) {
+            $cookie_time = time() + (60 * 60 * 24 * 30); // Set cookies for 30 days
+        }
+        else {
+            $cookie_time = 0; // Set cookies to expire when session ends (browser closes)
+        }
+
+        $user = $fs->getUserDetails($user_id);
+
+        // Set a couple of cookies
+        setcookie('flyspray_userid',   $user['user_id'], $cookie_time, '/');
+        setcookie('flyspray_passhash', crypt($user['user_pass'], $cookiesalt), $cookie_time, '/');
+
+        // If the user had previously requested a password change, remove the magic url
+        $remove_magic = $db->Query(
+                "UPDATE {$dbprefix}users SET magic_url = '' WHERE user_id = ?",
+                array($user['user_id'])
+            );
+
+        $_SESSION['SUCCESS'] = $authenticate_text['loginsuccessful'];
+    }
+}
+else {
+    // If the user didn't provide both a username and a password, show this error:
+    $_SESSION['ERROR'] = $authenticate_text['loginfailed'] . ' - ' . $authenticate_text['userandpass'];
+}
+$fs->redirect(Req::val('prev_page'));
 ?>
