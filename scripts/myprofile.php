4bf43651483ae8f9dfadd7161e5a66f04fddb913
MadCoder <MadCoder@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-30 10:16:09 +0000

    script/myprofile refactoring
    usual stuff

--- scripts/myprofile.php
+++ scripts/myprofile.php
@@ -9,26 +9,23 @@ $lang = $flyspray_prefs['lang_code'];
 $fs->get_language_pack($lang, 'admin');
 $fs->get_language_pack($lang, 'index');
 
-// This generates an URL so that the action script takes us back to the previous page
-$this_page = sprintf("%s",$_SERVER["REQUEST_URI"]);
-$this_page = str_replace('&', '&amp;', $this_page);
-
-if (isset($_COOKIE['flyspray_userid']))
-{
+if (!Cookie::has('flyspray_userid')) {
+    echo $admin_text['nopermission'];
+    exit;
+}
 
 echo '<h2>' . $language['editmydetails'] . '</h2>';
 
 ?>
-
 <fieldset class="admin">
-   <form action="<?php echo $conf['general']['baseurl'];?>index.php" method="post">
+   <form action="<?php echo $baseurl;?>index.php" method="post">
       <table class="admin">
          <tr>
             <td>
                <input type="hidden" name="do" value="modify" />
                <input type="hidden" name="action" value="edituser" />
                <input type="hidden" name="user_id" value="<?php echo $current_user['user_id'];?>" />
-               <input type="hidden" name="prev_page" value="<?php echo $this_page;?>" />
+               <input type="hidden" name="prev_page" value="<?php echo htmlentities($_SERVER['REQUEST_URI']);?>" />
 
                <label for="realname"><?php echo $admin_text['realname'];?></label>
                </td>
@@ -45,16 +42,15 @@ echo '<h2>' . $language['editmydetails'] . '</h2>';
          <tr>
             <td><label for="notifytype"><?php echo $admin_text['notifytype'];?></label></td>
             <td>
-            <?php if ($flyspray_prefs['user_notify'] == '1') { ?>
-            <select id="notifytype" name="notify_type">
-               <option value="0" <?php if ($current_user['notify_type'] == "0") {echo "selected=\"selected\"";};?>>None</option>
-               <option value="1" <?php if ($current_user['notify_type'] == "1") {echo "selected=\"selected\"";};?>>Email</option>
-               <option value="2" <?php if ($current_user['notify_type'] == "2") {echo "selected=\"selected\"";};?>>Jabber</option>
-            </select>
-            <?php
-            } else {
-               echo $admin_text['setglobally'];
-            }; ?>
+            <?php if ($flyspray_prefs['user_notify'] == '1'): ?>
+              <select id="notifytype" name="notify_type">
+                 <option value="0" <?php if ($current_user['notify_type'] == "0") echo 'selected="selected"'; ?>>None</option>
+                 <option value="1" <?php if ($current_user['notify_type'] == "1") echo 'selected="selected"'; ?>>Email</option>
+                 <option value="2" <?php if ($current_user['notify_type'] == "2") echo 'selected="selected"'; ?>>Jabber</option>
+              </select>
+            <?php else:
+                      echo $admin_text['setglobally'];
+                  endif; ?>
             </td>
          </tr>
          <tr>
@@ -69,14 +65,14 @@ echo '<h2>' . $language['editmydetails'] . '</h2>';
             <td><label for="tasks_perpage"><?php echo $admin_text['tasksperpage'];?></label></td>
             <td>
                 <select name="tasks_perpage" id="tasks_perpage">
-<?php
-// This should really share its list of values with admin.php...
-$perpagevals = array(10,25,50,100,250,500);
-foreach ($perpagevals as $n) {
-  $s = ($current_user['tasks_perpage'] == $n ? " selected=\"selected\"" : "");
-  echo "                     <option value=\"$n\"$s>$n</option>\n";
- }
-?>
+                <?php
+                    // This should really share its list of values with admin.php...
+                    $perpagevals = array(10,25,50,100,250,500);
+                    foreach ($perpagevals as $n) {
+                        $s = ($current_user['tasks_perpage'] == $n ? ' selected="selected"' : '');
+                        echo "<option value='$n'$s>$n</option>\n";
+                    }
+                ?>
                </select>
             </td>
          </tr>
@@ -97,10 +93,3 @@ foreach ($perpagevals as $n) {
       </table>
    </form>
 </fieldset>
-
-<?php
-} else
-{
-   echo $admin_text['nopermission'];
-}
-?>
