326ce83c74480ea0f7cf4d0c4c072bac5ac63f5e
MadCoder <MadCoder@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2005-10-30 10:16:46 +0000

    scripts/lostpw.php refactor
    usual things + avoid as much echo as possible, and be nearer a templating system

--- scripts/lostpw.php
+++ scripts/lostpw.php
@@ -1,6 +1,6 @@
 <?php
 
-   /*
+/*
    ------------------------------------------------------
    | This script allows users to request a notification |
    | that contains a link to a new password             |
@@ -10,67 +10,59 @@
 $lang = $flyspray_prefs['lang_code'];
 $fs->get_language_pack($lang, 'admin');
 
-  // Step One: user requests magic url
-  if (!isset($_GET['magic'])
-      && !isset($_COOKIE['flyspray_userid'])) {
+if (!Get::has('magic') && !Cookie::has('flyspray_userid')):     // Step One: user requests magic url
+?>
+    <h3><?php echo $admin_text['lostpw'] ?></h3>
+    <?php echo $admin_text['lostpwexplain'] ?>
+    <br /><br />
 
-    echo '<h3>' . $admin_text['lostpw'] . '</h3>' . "\n";
-    echo $admin_text['lostpwexplain'] . "\n";
+    <div class="admin">
+      <form action="<?php echo $baseurl ?>index.php" method="post">
+        <input type="hidden" name="do" value="modify" />
+        <input type="hidden" name="action" value="sendmagic" />
+        <b><?php echo $admin_text['username'] ?></b>
+        <input class="admintext" type="text" name="user_name" size="20" maxlength="20" />
+        <input class="adminbutton" type="submit" value="<?php echo $admin_text['sendlink'] ?>" />
+      </form>
+    </div>
+<?php
+elseif (!Get::has('magic') && !Cookie::has('flyspray_userid')): // Step Two: user enters new password
+    // Check that the magic url is valid
+    $check_magic = $db->Query(
+            "SELECT * FROM {$dbprefix}users WHERE magic_url = ?",
+            array($_GET['magic']));
 
-    echo '<br /><br />' . "\n";
+    if (!$db->CountRows($check_magic)) {
+        $_SESSION['ERROR'] = $admin_text['badmagic'];
+        $fs->redirect('./');
+    }
+?>
+    <h3><?php echo $admin_text['changepass'] ?></h3>
+    <br />
 
-    echo '<div class="admin">' . "\n";
-    echo '<form action="' . $conf['general']['baseurl'] . 'index.php" method="post">' . "\n";
-    echo '<input type="hidden" name="do" value="modify" />' . "\n";
-    echo '<input type="hidden" name="action" value="sendmagic" />' . "\n";
-    echo '<b>' . $admin_text['username'] . '</b>' . "\n";
-    echo '<input class="admintext" type="text" name="user_name" size="20" maxlength="20" />' . "\n";
-    echo '<input class="adminbutton" type="submit" value="' . $admin_text['sendlink'] . '" />' . "\n";
-    echo '</form>' . "\n";
-    echo '</div>' . "\n";
-
-
-  // Step Two: user enters new password
-} elseif (isset($_GET['magic'])
-          && !isset($_COOKIE['flyspray_userid']))
-{
-   // Check that the magic url is valid
-   $check_magic = $db->Query("SELECT * FROM {$dbprefix}users
-                              WHERE magic_url = ?",
-                              array($_GET['magic'])
-                            );
-
-   if (!$db->CountRows($check_magic))
-   {
-//       echo "<div class=\"redirectmessage\"><p><em>{$admin_text['badmagic']}</em></p></div>";
-//       echo '<meta http-equiv="refresh" content="2; URL=index.php">';
-      $_SESSION['ERROR'] = $admin_text['badmagic'];
-      $fs->redirect("./");
-
-   } else
-   {
-      echo '<h3>' . $admin_text['changepass'] . '</h3>' . "\n";
-
-      echo '<br />' . "\n";
-
-      echo '<form action="index.php" method="post">' . "\n";
-
-      echo '<table class="admin">' . "\n";
-      echo '<input type="hidden" name="do" value="modify" />' . "\n";
-      echo '<input type="hidden" name="action" value="chpass" />' . "\n";
-      echo '<input type="hidden" name="magic_url" value="' . $_GET['magic'] . '" />' . "\n";
-      echo '<tr><td><b>' . $admin_text['changepass'] . '</b></td>' . "\n";
-      echo '<td><input class="admintext" type="password" name="pass1" size="20" /></td></tr>' . "\n";
-      echo '<tr><td><b>' . $admin_text['confirmpass'] . '</b></td>' . "\n";
-      echo '<td><input class="admintext" type="password" name="pass2" size="20" /></tr>' . "\n";
-      echo '<tr><td></td><td><input class="adminbutton" type="submit" value="' . $admin_text['savenewpass'] . '" /></td></tr>' . "\n";
-      echo '</table>' . "\n";
-      echo '</form>' . "\n";
-      echo '</div>' . "\n";
-
-   // End of checking magic url validity
-   }
-
-// End of checking for magic url
-}
+    <div class="admin">
+    <form action="index.php" method="post">
+      <table>
+        <tr>
+          <td>
+            <b><?php echo $admin_text['changepass'] ?></b>
+            <input type="hidden" name="do" value="modify" />
+            <input type="hidden" name="action" value="chpass" />
+            <input type="hidden" name="magic_url" value="<?php echo Get::val('magic'); ?>" />
+          </td>
+          <td><input class="admintext" type="password" name="pass1" size="20" /></td>
+        </tr>
+        <tr>
+          <td><b><?php echo $admin_text['confirmpass'] ?></b></td>
+          <td><input class="admintext" type="password" name="pass2" size="20" /></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input class="adminbutton" type="submit" value="<?php echo $admin_text['savenewpass'] ?>" /></td>
+        </tr>
+      </table>
+    </form>
+    </div>
+<?php
+endif;                                                          // End of checking for magic url
 ?>
