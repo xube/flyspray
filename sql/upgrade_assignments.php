834c3b9adb2c39e3578a880b506e13f0e22836d6
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2006-08-25 14:22:02 +0000

    FS#1043: postgresql upgrade_assignments.php, bad arguments (rev 765)

--- sql/upgrade_assignments.php
+++ sql/upgrade_assignments.php
@@ -16,7 +16,13 @@ $db = new Database;
 $db->dbOpenFast($conf['database']);
 
 $db->Query("ALTER TABLE {assigned} DROP user_or_group");
-$db->Query("ALTER TABLE {assigned} CHANGE assignee_id user_id MEDIUMINT( 5 ) DEFAULT '0' NOT NULL");
+if (!strcasecmp($conf['database']['dbtype'], 'pgsql')) {
+    $db->Query('ALTER TABLE {assigned} ALTER assignee_id TYPE INT DEFAULT 0 NOT NULL');
+    $db->Query('ALTER TABLE {assigned} RENAME assignee_id TO user_id');
+} else {
+    $db->Query("ALTER TABLE {assigned} CHANGE assignee_id user_id MEDIUMINT( 5 ) DEFAULT '0' NOT NULL");
+}
+
 $db->Query("ALTER TABLE {assigned} ADD INDEX ( task_id , user_id )");
                           
 $check_sql = $db->Query("SELECT task_id, assigned_to
