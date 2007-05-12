79be87b92ac37a10f4006a8769e1d7f18a5288d0
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-05-12 09:20:38 +0000

    Another upgrade fix

--- setup/upgrade/0.9.9/upgrade_assignments.php
+++ setup/upgrade/0.9.9/upgrade_assignments.php
@@ -5,19 +5,25 @@
    | assignees per task as described in FS#329.  It only needs |
    | to be run once to do the conversion.                      |
    \***********************************************************/
-                         
-$check_sql = $db->Execute("SELECT task_id, assigned_to
+
+$check_sql = $db->Query("SELECT task_id, assigned_to
                            FROM {tasks}
                           WHERE assigned_to > '0'");
 
-while ($row = $check_sql->FetchRow())
+while ($row = $db->FetchRow($check_sql))
 {
-   $db->Execute('INSERT INTO {assigned}
+   $check = $db->Query('SELECT assigned_id FROM {assigned} WHERE task_id = ? AND user_id = ?',
+                       array($row['task_id'], $row['assigned_to']));
+   if ($db->FetchOne($check)) {
+       continue;
+   }
+
+   $db->Query('INSERT INTO {assigned}
                            (task_id, user_id)
                     VALUES (?,?)',
                            array($row['task_id'], $row['assigned_to']));
 
-   $db->Execute('UPDATE {tasks}
+   $db->Query('UPDATE {tasks}
                   SET assigned_to = 0
                 WHERE task_id = ?',
                       array($row['task_id']));
