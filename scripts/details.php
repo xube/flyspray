814c32df467e93f54d2971e3dd21c9d06b5503f2
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-09-08 07:48:37 +0000

    Hopefully fixed a slow query

--- scripts/details.php
+++ scripts/details.php
@@ -662,8 +662,8 @@ class FlysprayDoDetails extends FlysprayDo
 
             // Comments + cache
             $comments = $db->x->getAll('  SELECT c.*, ca.content FROM {comments} c
-                                LEFT JOIN {cache} ca ON (c.comment_id = ca.topic AND ca.type = ? AND c.last_edited_time <= ca.last_updated)
-                                    WHERE task_id = ?
+                                LEFT JOIN {cache} ca ON (c.comment_id = ca.topic AND ca.type = ?)
+                                    WHERE task_id = ? AND c.last_edited_time <= ca.last_updated
                                  ORDER BY date_added ASC',
                                    null, array('comm', $this->task['task_id']));
 
